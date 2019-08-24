1478731bfad701c71b1f3bbd9d79b92f434ad78d
ponychen123 <18709821294@outlook.com>
2019-08-24 15:49:21 +0800

    20190823 exercise

---
+++ ex0302.f90
@@ -0,0 +1,8 @@
+! Free Format
+program main
+write(*,*) "Hello"
+write(*,*) &
+"Hello"
+wri&
+&te(*,*) "Hello"
+end
---
+++ ex0405.f90
@@ -0,0 +1,5 @@
+program ex0405
+      integer a
+      a = 2+2*4-3
+      write(*,*) "2+2*4-3=",a
+end
